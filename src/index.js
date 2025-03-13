9cb05f54e276625d8cd5fe91f7e46e563777a089
Hernan-99 <hernan24744@gmail.com>
2025-03-13 11:25:03 -0300

    inicializacion de servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hola mundo");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Home</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriento en el puerto: http://localhost:8080`);
+});
